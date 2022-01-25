ee099aed71a76867ce04c327bde9d387db3e24af
Rado Radev <rado@auxnederlandbv.nl>
2022-01-25 20:48:10 +0200

    testing and demonstration, plus comments

--- rbtree2/main.cpp
+++ rbtree2/main.cpp
@@ -10,24 +10,55 @@ int main() {
     tree.insert(10, "Maria Bakalova");
     tree.insert(5, "Kosta Tsonev");
     tree.insert(20, "Georgi Mamalev");
+
+    cout << "Demonstrating tree with three nodes" << endl;
+    cout << "At this stage, it looks like a normal binary tree" << endl;
+    tree.print();
+
+    cout << "-------------" << endl << endl;
+
     tree.insert(30, "Teodora Duhovnikova");
     tree.insert(15, "Velko Kanev");
+    
+    cout << "It  still looks like a normal binary tree, besides some recoloring" << endl;
+    tree.print();
+
+    cout << "-------------" << endl << endl;
+
     tree.insert(25, "Konstantin Kotsev");
     tree.insert(1, "Georgi Kaloyanchev");
     tree.insert(50, "Georgi Vachkov");
+
+    cout << "Still no movements" << endl;
     tree.print();
 
-    cout << "-------------" << endl;
+    cout << "-------------" << endl << endl;
+
+    tree.erase(15);
+    cout << "Removing 15 makes it rearrange and recolor" << endl;
+    cout << "Delete node since its external" << endl;
+    cout << "Double-blackness found, which can be fixed with one left transformation" << endl;
+    tree.print();
+
+    cout << "-------------" << endl << endl;
+
+    cout << "Node to delete(30) has two children" << endl;
+    cout << "Find the right-most node in the left subtree of the delete node" << endl;
+    cout << "And swap it with the delete node" << endl;
+    cout << "and remove the right-most" << endl;
     tree.erase(30);
-
     tree.print();
 
-    cout << "-------------" << endl;
+    cout << "-------------" << endl << endl;
 
+    tree.insert(14, "Stefan Danailov");
+    cout << "Simple binary tree insert, when adding 14" << endl;
+    tree.print();
+
+    cout << "-------------" << endl << endl;
+
+    cout << "Demonstrating find capabilities" << endl;
     cout << tree.find(5)->value() << endl;
-
-    cout << "-------------" << endl;
-
-    cout << tree.begin()->value() << endl;
+        
     return 0;
 }
--- rbtree2/red-black-tree.h
+++ rbtree2/red-black-tree.h
@@ -27,6 +27,9 @@ class RBTree {
         class Node {
             private:
                 Node() : entry(), left(nullptr), right(nullptr), parent(nullptr) {}
+                Node(const K& k, const V& v, Node* l = nullptr, Node* r = nullptr, Node* p = nullptr)
+                    : entry(k, v), left(l), right(r), parent(p) {}
+
                 RBEntry entry;
                 Node* parent;
                 Node* left;
@@ -39,7 +42,7 @@ class RBTree {
         ~RBTree();
         int size() const { return n; }
         bool empty() const { return size() == 0; }
-        Position root() const { return _root; }
+        Position root() const { return Position(_root); }
         Position begin() const;
         Position find(const K& k) const;
         void insert(const K& k, const V& v);
@@ -50,9 +53,9 @@ class RBTree {
 
         // this algorithm is demonstrated in the 
         // documentation under Self-balancing Binary Search Trees
-        void leftTransform(const Position& pos);
-        void rightTransform(const Position& pos);
-        void swap(const Position& u, const Position& v);
+        void leftTransform(Position pos);
+        void rightTransform(Position pos);
+        void swap(Position u, Position v);
         Position minInLeftSubTree(const Position& pos) const;
         void print() const;
         void print(const Position& pos, string ind, bool last) const;
@@ -64,9 +67,9 @@ class RBTree {
     public:
         class Position {
             public:
-                Position() : node(nullptr) {}
-                Position(Node* node) : node(node) {}
-                Position(const Position& pos) : node(pos.node) {}
+                Position() : node(nullptr) { }
+                Position(Node* node) : node(node) { }
+                Position(const Position& pos) : node(pos.node) { }
                 Position& operator =(const Position& pos) { node = pos.node; return *this; }
                 bool operator ==(const Position& p) const { return node == p.node; }
                 bool operator !=(const Position& p) const { return node != p.node; }
@@ -83,9 +86,9 @@ class RBTree {
                 Position parent() const { return Position(node->parent); }
                 Position left() const { return Position(node->left); }
                 Position right() const { return Position(node->right); }
-                void setParent(Position chNode){ node->parent = chNode.node; }
-                void setLeft(Position chNode){ node->left = chNode.node; }
-                void setRight(Position chNode){ node->right = chNode.node; }
+                void setParent(const Position& chNode) { node->parent = chNode.node; }
+                void setLeft(const Position& chNode) { node->left = chNode.node; }
+                void setRight(const Position& chNode) { node->right = chNode.node; }
             private:
                 Node* node;
 
@@ -119,32 +122,28 @@ typename RBTree<E>::Position RBTree<E>::begin() const {
 template <typename E>
 void RBTree<E>::insert(const K& k, const V& v) {
     // Insert as every basic Binary Search Tree does
-    Node* node = new Node();
-    node->left = RBNULL;
-    node->right = RBNULL;
-    node->entry.setKey(k);
-    node->entry.setValue(v);
+    Node* node = new Node(k, v, RBNULL, RBNULL);
     node->entry.setColor(RED);
 
     Position pos(node);
 
     Position y(nullptr);
-    Position x(_root);
+    Position rootPos(_root);
 
     // find position to add insert the node
-    while(x != Position(RBNULL)) {
-        y = x;
+    while(rootPos != Position(RBNULL)) {
+        y = rootPos;
         // if smaller, go to left
-        if(pos < x) {
-            x = x.left();
+        if(pos < rootPos) {
+            rootPos = rootPos.left();
         } else { // otherwise, go to right >=
-            x = x.right();
+            rootPos = rootPos.right();
         }
     }
 
     // y is the parent of x
     pos.setParent(y);
-    if(!y.exist()) {
+    if(!y.exist()) { // if the new entry is positioned as a root
         _root = pos.node;
     } else if(pos < y) {
         y.setLeft(pos);
@@ -155,54 +154,43 @@ void RBTree<E>::insert(const K& k, const V& v) {
     // The tree is empty, in this case pos is set to be root and it is painted black.
     if(!pos.parent().exist()) { // if the new node happens to be the root node
         pos->setColor(BLACK);
-    } else if(pos.parent().parent().exist()) {
-        restructureAfterInsert(pos);
+    } else if(pos.parent().parent().exist()) { // if there is a grandmonther
+        restructureAfterInsert(pos); // fix the tree, 
+        // making sure all red-black tree properties are met
     }
+    n++;
 }
 
 template <typename E>
 void RBTree<E>::erase(const Position& pos) {
+    // simply use the other function calling it by key
     erase(pos->key());
 }
 
 template <typename E>
 void RBTree<E>::erase(const K& key) {
-    Position rootPos(_root);
-    Position z(RBNULL);
+    Position rootPos(_root); // root position
+    Position z = find(key); // find the entry to delete
     Position x(new Node);
-    Position y(new Node);
+    Position y(z);
 
-    while (rootPos != Position(RBNULL)){
-        if (rootPos->key() == key) {
-            z = rootPos;
-        }
-
-        if (rootPos->key() <= key) {
-            rootPos = rootPos.right();
-        } else {
-            rootPos = rootPos.left();
-        }
-    }
-
-    if (z == RBNULL) {
-        throw TreeExcept("Couldn't find key in the tree");
-    } 
-
-    y = z;
-    Color y_original_color = y->color();
-    if (z.left() == Position(RBNULL)) {
+    Color yFirstColor = y->color();
+    if (z.left() == Position(RBNULL)) { // if z's left child is external
         x = z.right();
-        swap(z, z.right());
+        swap(z, x); // in this case, simply replace z with its right child
     } else if (z.right() == Position(RBNULL)) {
         x = z.left();
-        swap(z, z.left());
+        swap(z, x); // in this case, simply replace z with its left child
     } else {
+        // find the leftmost key
         y = minInLeftSubTree(z.right());
-        y_original_color = y->color();
+        yFirstColor = y->color();
         x = y.right();
+
+        // if y is a child of z
         if (y.parent() == z) {
             x.setParent(y);
-        } else {
+        } else { 
             swap(y, y.right());
             y.setRight(z.right());
             y.right().setParent(y);
@@ -214,31 +202,39 @@ void RBTree<E>::erase(const K& key) {
         y->setColor(z->color());
     }
     delete z.node;
-    if (y_original_color == BLACK){
+    if (yFirstColor == BLACK){
         restructureAfterDelete(x);
     }
+    n--;
 }
 
 template <typename E>
 typename RBTree<E>::Position RBTree<E>::find(const K& key) const {
-    Position rootPos(_root);
+    // The usual search procedure in a binary search tree
+    // Starting from the root
+    Position current(_root);
     Position z(RBNULL); // initially empty node
 
-    while (rootPos != Position(RBNULL)){
-        if (rootPos->key() == key) {
-            z = rootPos;
+    // move down until external node is encountered
+    while (current != Position(RBNULL)){
+        // check if this is the key, otherwise move left or right
+        if (current->key() == key) {
+            z = current;
         }
-
-        if (rootPos->key() <= key) {
-            rootPos = rootPos.right();
-        } else {
-            rootPos = rootPos.left();
+        
+        // If smaller, go to right
+        if (current->key() <= key) {
+            current = current.right();
+        } else { // otherwise, go to left
+            current = current.left();
         }
     }
 
+    // If external node, throw exception for missing key
     if (z == RBNULL) {
         throw TreeExcept("Couldn't find key in the tree");
     } else {
+        // otherwise, return the position of the key
         return Position(z);
     }
 }
@@ -246,61 +242,71 @@ typename RBTree<E>::Position RBTree<E>::find(const K& key) const {
 template <typename E>
 void RBTree<E>::restructureAfterDelete(Position& pos) {
     Position s(new Node);
+    // move up until the root or until black node
     while (pos != Position(_root) && pos.isBlack()) {
         if (pos == pos.parent().left()) {
             s = pos.parent().right();
+            // If pos's sister is a red node, simply delete it, 
+            // because removing red nodes, doesn't break the rules
             if (s.isRed()) {
-                // case 3.1
+                // 3.1 in the documentation
                 s->setColor(BLACK);
                 pos.parent()->setColor(RED);
                 leftTransform(pos.parent());
                 s = pos.parent().right();
             }
 
+            // if both children are black
             if (s.left().isBlack() && s.right().isBlack()) {
-                // case 3.2
+                // 3.2 in the documentation
                 s->setColor(RED);
                 pos = pos.parent();
             } else {
+                // if only the right is black
                 if (s.right().isBlack()) {
-                    // case 3.3
+                    // 3.3 in the documentation
                     s.left()->setColor(BLACK);
                     s->setColor(RED);
                     rightTransform(s);
                     s = pos.parent().right();
                 } 
 
-                // case 3.4
+                // if only the right is red
+                // 3.4 in the documentation
                 s->setColor(pos.parent()->color());
                 pos.parent()->setColor(BLACK);
                 s.right()->setColor(BLACK);
                 leftTransform(pos.parent());
                 pos.node = _root;
             }
-        } else {
+        } else { // symmetric to above
             s = pos.parent().left();
+            // If pos's sister is a red node, simply delete it, 
+            // because removing red nodes, doesn't break the rules
             if (s.isRed()) {
-                // case 3.1
+                // 3.1 in the documentation
                 s->setColor(BLACK);
                 pos.parent()->setColor(RED);
                 rightTransform(pos.parent());
                 s = pos.parent().left();
             }
 
+            // if both children are black
             if (s.right().isBlack() && s.right().isBlack()) {
-                // case 3.2
+                // 3.2 in the documentation
                 s->setColor(RED);
                 pos = pos.parent();
             } else {
+                // if only the right is black
                 if (s.left().isBlack()) {
-                    // case 3.3
+                    // 3.3 in the documentation
                     s.right()->setColor(BLACK);
                     s->setColor(RED);
                     leftTransform(s);
                     s = pos.parent().left();
                 } 
-
-                // case 3.4
+                // if only the right is red
+                // 3.4 in the documentation
                 s->setColor(pos.parent()->color());
                 pos.parent()->setColor(BLACK);
                 s.left()->setColor(BLACK);
@@ -313,15 +319,16 @@ void RBTree<E>::restructureAfterDelete(Position& pos) {
 }
 
 template <typename E>
-void RBTree<E>::swap(const Position& u, const Position& v) {
+void RBTree<E>::swap(Position u, Position v) {
+    // a bit special case when the upper entry is the current root
     if (!u.parent().exist()) {
-        _root = v.node;
+        _root = v.node; 
     } else if (u == u.parent().left()){
         u.parent().setLeft(v);
     } else {
         u.parent().setRight(v);
     }
-    v.parent() = u.parent();
+    v.setParent(u.parent());
 }
 
 template <typename E>
@@ -334,9 +341,9 @@ typename RBTree<E>::Position RBTree<E>::minInLeftSubTree(const Position& pos) co
 }
 
 template <typename E>
-void RBTree<E>::leftTransform(const Position& pos) {
+void RBTree<E>::leftTransform(Position pos) {
     Position y = pos.right(); // the node on the right
-    pos.right() = y.left(); // move b to the right of pos
+    pos.setRight(y.left()); // move b to the right of pos
     if (y.left() != Position(RBNULL)) { // if left child of y is not empty
         y.left().setParent(pos); // set the parent to the pos
     }
@@ -349,13 +356,13 @@ void RBTree<E>::leftTransform(const Position& pos) {
         pos.parent().setRight(y);
     }
     y.setLeft(pos); // put pos to the left of y
-    pos.parent() = y;
+    pos.setParent(y);
 }
 
 template <typename E>
-void RBTree<E>::rightTransform(const Position& pos) {
+void RBTree<E>::rightTransform(Position pos) {
     Position y = pos.left(); // the node on the left
-    pos.left() = y.right(); // move b to the left of pos
+    pos.setLeft(y.right()); // move b to the left of pos
     if (y.right() != Position(RBNULL)) { // if right child of y is not empty
         y.right().parent() = pos; // set the parent to the pos
     }
@@ -368,7 +375,7 @@ void RBTree<E>::rightTransform(const Position& pos) {
         pos.parent().setLeft(y);
     }
     y.setRight(pos); // put pos to the right of y
-    pos.parent() = y;
+    pos.setParent(y);
 }
 
 template <typename E>
@@ -378,38 +385,38 @@ void RBTree<E>::restructureAfterInsert(Position& pos) {
         if (pos.parent() == pos.parent().parent().right()) {
             aunt = pos.parent().parent().left();
             if (aunt.isRed()) {
-                // case 3.1
+                // 3.1 in the documentation
                 aunt->setColor(BLACK);
                 pos.parent()->setColor(BLACK);
                 pos.parent().parent()->setColor(RED);
                 pos = pos.parent().parent();
             } else {
                 if (pos == pos.parent().left()) {
-                    // case 3.2.2
+                    // 3.2.2 in the documentation
                     pos = pos.parent();
                     rightTransform(pos);
                 }
-                // case 3.2.1
+                // 3.2.1 in the documentation
                 pos.parent()->setColor(BLACK);
                 pos.parent().parent()->setColor(RED);
                 leftTransform(pos.parent().parent());
             }
-        } else {
+        } else { // symmetric
             aunt = pos.parent().parent().right();
 
             if (aunt.isRed()) {
-                // mirror case 3.1
+                // 3.1 in the documentation
                 aunt->setColor(BLACK);
                 pos.parent()->setColor(BLACK);
                 pos.parent().parent()->setColor(RED);
                 pos = pos.parent().parent();	
             } else {
                 if (pos == pos.parent().right()) {
-                    // mirror case 3.2.2
+                    // 3.2.2 in the documentation
                     pos = pos.parent();
                     leftTransform(pos);
                 }
-                // mirror case 3.2.1
+                // 3.2.1 in the documentation
                 pos.parent()->setColor(BLACK);
                 pos.parent().parent()->setColor(RED);
                 rightTransform(pos.parent().parent());
